ce1706773e9ebb3ed9acc60cbf415a02c5501693
Razi <12abdulrahim21@gmail.com>
2023-07-08 17:59:09 +0530

    next auth with google done

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreatePrompt
+ = () => {
+  return (
+    <div>CreatePrompt
+        
+    </div>
+  )
+}
+
+export default CreatePrompt
--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "lh3.googleusercontent.com",
+            port: "",
+        },
+    ],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
